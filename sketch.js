e8337c442400094840200ce5acf594ccab577e24
Holaaaaaaaaaaaaa <92826508+Holaaaaaaaaaaaaa@users.noreply.github.com>
2021-10-19 15:59:39 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(600, 400);
+  //canvas.parent("lienzo");
+  background(220);
+}
+
+function draw() {
+  fill(random (0),random (255),random (216));
+  textSize(40);
+  text("AAAAAA",mouseX,mouseY);
+}
